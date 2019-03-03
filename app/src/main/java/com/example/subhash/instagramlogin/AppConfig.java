cfae201ab1dd250d12d5e824906f27acbec85f49
SubhashDeveloper <subhash148sv@gmail.com>
2019-03-04 01:37:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/subhash/instagramlogin/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.subhash.instagramlogin;
+
+public class AppConfig {
+    public static String CLIENT_ID;
+    public static String CLIENT_SECRET;
+    public static String CALLBACK_URL;
+}
